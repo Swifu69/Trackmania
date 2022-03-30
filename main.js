c0bcb7e8f80618f9ea1f0cca8912e8c49911a4b3
Johan Borgersen <75065342+Swifu69@users.noreply.github.com>
2022-03-30 14:29:18 +0200

    pls rewiew bby?
    husk: npm i path
    
    Pls bby i wanna see your bobs

--- main.js
+++ main.js
@@ -1,8 +1,9 @@
 const express = require("express");
+const path = require('path')
 const app = express();
 
 app.set("view engine","ejs")
-app.use(express.static("public"));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get("/", function(req,res){
     res.render("index")
